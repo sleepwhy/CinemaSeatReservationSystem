c2fcb507d6d0eded60bec3704a979e1d8591f6b4
sleepwhy <avrol928@gmail.com>
2025-03-22 23:45:33 +0300

    Update FormMain.Designer.cs

--- WindowsFormsAppH6/FormMain.Designer.cs
+++ WindowsFormsAppH6/FormMain.Designer.cs
@@ -160,7 +160,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 302);
+            this.ClientSize = new System.Drawing.Size(388, 342);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.radioButton1);
