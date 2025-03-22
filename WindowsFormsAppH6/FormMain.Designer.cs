65c05ad687be611e97afa0602fd8baa8eb31af94
sleepwhy <avrol928@gmail.com>
2025-03-22 23:09:40 +0300

    First Commit

---
+++ WindowsFormsAppH6/FormMain.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsAppH6
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSatirSayi = new System.Windows.Forms.TextBox();
+            this.textSutunSayi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(93, 207);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sinema Salonu Oluştur";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 397);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seçilen Koltuklar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(107, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "AAA";
+            // 
+            // textSatirSayi
+            // 
+            this.textSatirSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSatirSayi.Location = new System.Drawing.Point(186, 43);
+            this.textSatirSayi.Name = "textSatirSayi";
+            this.textSatirSayi.Size = new System.Drawing.Size(100, 38);
+            this.textSatirSayi.TabIndex = 4;
+            // 
+            // textSutunSayi
+            // 
+            this.textSutunSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSutunSayi.Location = new System.Drawing.Point(186, 99);
+            this.textSutunSayi.Name = "textSutunSayi";
+            this.textSutunSayi.Size = new System.Drawing.Size(100, 38);
+            this.textSutunSayi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.Location = new System.Drawing.Point(13, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Satır Sayısı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Sütun Sayısı:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(172, 160);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 22);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.Text = "Evet";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label5.Location = new System.Drawing.Point(12, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Silinecek Koltuklar Var:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(228, 161);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 22);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hayır";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 302);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textSutunSayi);
+            this.Controls.Add(this.textSatirSayi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMain";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textSatirSayi;
+        private System.Windows.Forms.TextBox textSutunSayi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ WindowsFormsAppH6/FormMain.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppH6
+{
+    public partial class FormMain : Form
+    {
+
+        public FormMain()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            label2.Visible = false;
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog; 
+            this.MaximizeBox = false;  
+            this.MinimizeBox = false;  
+        }
+
+        public void ChangeBackgroundColor(Color newColor)
+        {
+            this.BackColor = newColor;
+            this.Invalidate();  
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.Blue, Color.LightSkyBlue, 45F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);  
+            }
+        }
+
+
+        List<string> secilenKoltuklar = new List<string>();
+
+
+
+
+
+        private void SinemaKoltuklariOlustur(int sutunSayisi, int satirSayisi, string bosKonumlar,
+                            string satilmisKoltuklar)
+        {
+            var satilmisKoltuks = satilmisKoltuklar.Split(',');
+            var bosKoltuks = bosKonumlar.Split(',');
+
+            List<string> bosKoltuklar = bosKoltuks.ToList();
+
+
+            for (int i = 0; i < bosKoltuks.Length; i++)
+            {
+                if (bosKoltuks[i].Contains("-"))
+                {
+                    var alan = bosKoltuks[i].Split('-');
+
+                    char baslangicHarf = alan[0][0];
+                    char bitisHarf = alan[1][0];
+                    int harfFarki = Math.Abs(baslangicHarf - bitisHarf) + 1;
+
+                    int baslangicSayi = Convert.ToInt32(alan[0].Substring(1));
+                    int bitisSayi = Convert.ToInt32(alan[1].Substring(1));
+
+                    for (int j = 0; j < harfFarki; j++)
+                    {
+                        for (int k = baslangicSayi; k <= bitisSayi; k++)
+                        {
+                            string yeniKoltuk = ((char)(baslangicHarf + j)).ToString() + k.ToString();
+                            bosKoltuklar.Add(yeniKoltuk);
+                        }
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < satirSayisi; i++)
+            {
+                for (int j = 0; j < sutunSayisi; j++)
+                {
+                    string koltukAdi = (char)(65 + i) + (j+1).ToString();
+
+                    if (!bosKoltuklar.Contains(koltukAdi))
+                    {
+                        Button btn = new Button();
+                        btn.Size = new Size(50, 50);
+                        btn.Text = koltukAdi;
+                        btn.Location = new Point(55 * j, 55 * i);
+                        if (satilmisKoltuks.Contains(koltukAdi))
+                        {
+                            btn.Enabled = false;
+
+                        }
+                        else
+                        {
+                            btn.BackColor = Color.LightGreen;
+                            btn.MouseEnter += new EventHandler(buttonEnter);
+                            btn.MouseLeave += new EventHandler(buttonLeave);
+                            btn.MouseClick += new MouseEventHandler(buttonSecildi);
+                            btn.KeyDown += new KeyEventHandler(buttonKey);
+                        }
+                        btn.Image = Properties.Resources.seat;
+                        btn.TextAlign = ContentAlignment.BottomCenter;
+                        btn.ImageAlign = ContentAlignment.TopCenter;
+
+
+                        this.Controls.Add(btn);
+
+
+                    }
+                    ChangeBackgroundColor(Color.LightYellow);
+                }
+            }
+
+            label1.Location = new Point(55 * sutunSayisi, 55);
+            label2.Location = new Point(55 * sutunSayisi, 85);
+
+            ogeleriGorunmezYap();
+        }
+        
+        private void ogeleriGorunmezYap()
+        {
+            label1.Visible = true;
+            button2.Visible = false;
+            label5.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+
+            if(txtSilinecekKoltuklar != null)
+            {
+                Controls.Remove(txtSilinecekKoltuklar);
+                txtSilinecekKoltuklar.Dispose();
+            }
+            if(dataGridView != null)
+            {
+                Controls.Remove(dataGridView);
+                dataGridView.Dispose();
+            }
+            if(btnEkle != null)
+            {
+                Controls.Remove(btnEkle);
+                btnEkle.Dispose();
+            }
+
+        }
+
+        private void buttonEnter(object sender, EventArgs e)
+        {
+            Button tiklananButton = (Button)sender;
+
+
+            if (!secilenKoltuklar.Contains(tiklananButton.Text))
+            {
+                tiklananButton.BackColor = Color.Blue;
+            }
+            
+        }
+
+        private void buttonLeave(object sender , EventArgs e)
+        {
+            Button tiklananButton = (Button)sender;
+            if (!secilenKoltuklar.Contains(tiklananButton.Text))
+            {
+                tiklananButton.BackColor = Color.LightGreen;
+            }
+            
+        }
+
+        private void buttonSecildi(object sender, MouseEventArgs e)
+        {
+            Button tiklananButton = (Button)sender;
+
+            if(tiklananButton.BackColor == Color.LightGreen || tiklananButton.BackColor == Color.Blue)
+            {
+                tiklananButton.BackColor = Color.Red;
+                secilenKoltuklar.Add(tiklananButton.Text);
+            }
+            else
+            {
+                tiklananButton.BackColor = Color.LightGreen;
+                secilenKoltuklar.Remove(tiklananButton.Text);
+            }
+            secilenKoltuklariGuncelle();
+        }
+
+        private void buttonKey(object sender, KeyEventArgs e)
+        {
+            Button tiklananButton = (Button)sender;
+
+            if(e.KeyCode == Keys.Space && !secilenKoltuklar.Contains(tiklananButton.Text))
+            {
+                secilenKoltuklar.Add(tiklananButton.Text);
+                tiklananButton.BackColor = Color.Red;
+            }
+            else if(e.KeyCode == Keys.Space && secilenKoltuklar.Contains(tiklananButton.Text))
+            {
+                secilenKoltuklar.Remove(tiklananButton.Text);
+                tiklananButton.BackColor = Color.LightGreen;
+            }
+            secilenKoltuklariGuncelle();
+        }
+
+        private void secilenKoltuklariGuncelle()
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+
+            label2.Text = "";
+            for(int i = 0;i < secilenKoltuklar.Count; i++)
+            {
+                label2.Text += secilenKoltuklar[i] + ", ";
+                if((i+1) % 4 == 0) { label2.Text += "\n"; }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int satirSayi = Convert.ToInt32(textSatirSayi.Text);
+                int sutunSayi = Convert.ToInt32(textSutunSayi.Text);
+
+
+                if(satirSayi == 0 || sutunSayi == 0)
+                {
+                    MessageBox.Show("Lütfen Geçerli Sayılar giriniz!");
+                }
+                else if(satirSayi > 18 || sutunSayi > 31)
+                {
+                    MessageBox.Show("Lütfen Daha Düşük Rakamlar Giriniz!\n" +
+                        "Satır Sayısı(MAX) = 18\n" +
+                        "Sütun Sayısı(MAX) = 31");
+                }
+                else if (satirSayi < 3 || sutunSayi < 3)
+                {
+                    MessageBox.Show("Lütfen Daha Büyük Rakamlar Giriniz\n" +
+                        "Satır Sayısı(MAX) = 18\n" +
+                        "Sütun Sayısı(MAX) = 31");
+                }
+                else
+                {
+
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    textSatirSayi.Visible = false;
+                    textSutunSayi.Visible = false;
+                    List<string> silinecekKoltuklar = new List<string>();
+                    if (dataGridView != null)
+                    {
+                        foreach (DataGridViewRow row in dataGridView.Rows)
+                        {
+                            if (!row.IsNewRow)
+                            {
+                                silinecekKoltuklar.Add(row.Cells["Silinecekler"].Value?.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        silinecekKoltuklar.Add(" ");
+                    }
+
+
+
+                    string silinecekler = string.Join(",", silinecekKoltuklar);
+
+                    SinemaKoltuklariOlustur(sutunSayi, satirSayi, silinecekler, "A1,A2,F5,F6,G9,G10,G11,H1,H12,A11,A12");
+                    
+                    if(sutunSayi > 20  || satirSayi > 15)
+                    {
+                        WindowState = FormWindowState.Maximized;
+                    }
+                    else
+                    {
+                        Height = satirSayi * 55 + 55;
+                        Width = sutunSayi * 55 + 250;
+                    }
+                }
+                    
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Lütfen Sadece Rakam Giriniz!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+
+
+
+            
+        }
+
+        DataGridView dataGridView;
+        Button btnEkle;
+        private void dataGridViewiOlustur()
+        {
+            dataGridView = new DataGridView();
+            dataGridView.Size = new Size(140, 250);
+            dataGridView.Location = new Point(320, 20);
+            dataGridView.Columns.Add("Silinecekler", "Silinecek Koltuklar");
+            dataGridView.ScrollBars = ScrollBars.Vertical;
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 14, FontStyle.Bold);
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Controls.Add(dataGridView);
+            
+        }
+
+        private TextBox txtSilinecekKoltuklar;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                txtSilinecekKoltuklar = new TextBox();
+                txtSilinecekKoltuklar.Location = new Point(180, 200);
+
+                this.Controls.Add(txtSilinecekKoltuklar);
+                button2.Top += 50;
+                dataGridViewiOlustur();
+                Size = new Size(this.Width + 100, this.Height);
+
+
+                btnEkle = new Button();
+                btnEkle.Location = new Point(90, 199);
+                btnEkle.Text = "Ekle";
+                ekleButonuOzellikleriniAyarla();
+                Controls.Add(btnEkle);
+            }
+            ChangeBackgroundColor(Color.White);
+        }
+
+        private void ekleButonuOzellikleriniAyarla()
+        {
+            btnEkle.Click += new EventHandler(ekleButonuTiklandi);
+        }
+
+        private void ekleButonuTiklandi(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtSilinecekKoltuklar.Text))
+            {
+                dataGridView.Rows.Add(txtSilinecekKoltuklar.Text);
+                txtSilinecekKoltuklar.Clear();
+            }
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked && txtSilinecekKoltuklar != null && btnEkle != null)
+            {
+                this.Controls.Remove(txtSilinecekKoltuklar);
+                txtSilinecekKoltuklar.Dispose();
+                button2.Top -= 50;
+
+                Controls.Remove(btnEkle);
+                btnEkle.Dispose();
+
+                this.Controls.Remove(dataGridView);
+                dataGridView.Dispose();
+                Size = new Size(this.Width - 100, this.Height);
+
+
+            }
+            ChangeBackgroundColor(Color.White);
+        }
+    }
+}
